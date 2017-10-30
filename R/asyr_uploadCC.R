25964ed34b1af18db1f43fa4455e0b5e5552f985
JARS3N <JARS3N@users.noreply.github.com>
2017-10-29 21:01:46 -0400

    Update asyr_uploadCC.R

--- R/asyr_uploadCC.R
+++ R/asyr_uploadCC.R
@@ -6,18 +6,17 @@ UploadsCC<-function(u){
     "W" = "xfe96wetqc",
     "Q" = "xf24legacy"
   )
-  #ConnectInfo<-DataStash::Triton()
-  my_db <- rmysqlCon()
+  my_db <- adminKraken::con_mysql()
   u<-mutate(u,type=sapply(Lot,function(u){substr(u,1,1)}))
   un.u<-unique(u$type)
   if (length(un.u)==1){
-      dbWriteTable(my_db, name=unname(platstring[un.u]),value=select(u,-type),
-                   append=TRUE,overwrite = FALSE,row.names=FALSE)
+    dbWriteTable(my_db, name=unname(platstring[un.u]),value=select(u,-type),
+                 append=TRUE,overwrite = FALSE,row.names=FALSE)
     dbDisconnect(my_db)
   }else{
-      dbWriteTable(my_db, name=unname(platstring[un.u[1]]),
-                   value=select(filter(u,type== un.u[1]),-type),
-                   append=TRUE,overwrite = FALSE,row.names=FALSE)
+    dbWriteTable(my_db, name=unname(platstring[un.u[1]]),
+                 value=select(filter(u,type== un.u[1]),-type),
+                 append=TRUE,overwrite = FALSE,row.names=FALSE)
     dbDisconnect(my_db)
     UploadsCC(filter(u,type!= un.u[1]))
   }
