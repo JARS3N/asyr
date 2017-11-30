050881487ef00af353e03b64987824d7bce813fd
JARS3N <JARS3N@users.noreply.github.com>
2017-11-30 11:51:29 -0500

    adjusting for wetqc/combo assay

--- R/asyr_extract_wetQC.R
+++ R/asyr_extract_wetQC.R
@@ -1,19 +1,22 @@
-extract_wetQC<-function(E){
-  if(E$assay=='ksv'){
-    E$ksv[c('Gain','sorpH')]<-NA
+extract_wetQC <- function (E) 
+{
+  output_name_order<-c("Well", "sorpH", "O2.LED", "O2.CalEmission", "O2.Status", 
+                       "pH.LED", "pH.CalEmission", "pH.Status", "Target", "Gain", 
+                       "Ambient", "F0", "KSV", "Inst", "sn", "Lot")
+  if (E$assay== 'wetqc'){
+    return(E$wetqc[output_name_order])
   }
-  if(E$assay=='gain'){
-    E$gain[c('KSV','F0','Ambient')]<-NA
+  if (E$assay == "ksv") {
+    E$ksv[c("Gain", "sorpH")] <- NA
   }
-  E[[E$assay]][c('pH.LED','pH.CalEmission',"pH.IntialReferenceDelta",
-                 "pH.Status","O2.LED","O2.CalEmission","O2.IntialReferenceDelta","O2.Status")  
-               ]<-NULL
-  OUT<-merge(E$CAL,E[[E$assay]],by='Well')
-  OUT[c("Lot","sn","Inst","Target")]<-list(E$Lot,E$sn,E$Inst,E$PH_COEF$target)
-  OUT[c(
-    "Well","sorpH",
-    "O2.LED","O2.CalEmission","O2.Status",
-    "pH.LED","pH.CalEmission","pH.Status","Target",
-    "Gain","Ambient","F0","KSV","Inst","sn","Lot")
-    ]
+  if (E$assay == "gain") {
+    E$gain[c("KSV", "F0", "Ambient")] <- NA
+  }
+  E[[E$assay]][c("pH.LED", "pH.CalEmission", "pH.IntialReferenceDelta", 
+                 "pH.Status", "O2.LED", "O2.CalEmission", "O2.IntialReferenceDelta", 
+                 "O2.Status")] <- NULL
+  OUT <- merge(E$CAL, E[[E$assay]], by = "Well")
+  OUT[c("Lot", "sn", "Inst", "Target")] <- list(E$Lot, E$sn, 
+                                                E$Inst, E$PH_COEF$target)
+  return(OUT[output_name_order])
 }
