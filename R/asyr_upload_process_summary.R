bf6b525c3e74bea5e09abf588b579d612736a95d
JARS3N <JARS3N@users.noreply.github.com>
2017-12-01 10:38:58 -0500

    Create asyr_upload_process_summary.R

---
+++ R/asyr_upload_process_summary.R
@@ -0,0 +1,21 @@
+upload_process_summary<-function(u){
+  require(RMySQL)
+  my_db <- adminKraken::con_mysql()
+  platstring<-c(
+    "C" ="xfpwetqc",
+    "B" = "xfe24wetqc",
+    "W" = "xfe96wetqc",
+    "Q" = "xf24legacy"
+  )
+  types<-sapply(Lot,function(u){substr(u,1,1)})
+  u$table <- platstring[types]
+  u$user <- Sys.getenv('USERNAME')
+  u$date <- Sys.time()
+  dbWriteTable(my_db,
+               name='wetqcTransactions',
+               value=u,
+               overwrite=F,
+               append=T
+  )
+
+}
