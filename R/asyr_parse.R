2d4ba11bf4cfbc2d8ed4ef14a4ed37ab6665d8eb
JARS3N <JARS3N@users.noreply.github.com>
2018-03-09 13:39:26 -0500

    Update asyr_parse.R

--- R/asyr_parse.R
+++ R/asyr_parse.R
@@ -15,6 +15,9 @@ parse<-function(X){
   E$cal_temp <- asyr::cal_temp(X)
   E$O2_COEF$F0<-asyr::f0(E$O2_COEF$Ksv,E$O2_COEF$target,E$cal_temp)
   E$LVL$mmHgO2<-(1/E$O2_COEF$Ksv) * ((E$O2_COEF$F0/E$LVL$O2_CorrectedEmission)-1)
+  E$LVL<-merge(E$LVL,E$CAL[c('Well','pH.CalEmission')],by='Well')
+  E$LVL$pH<-(7400 - ((E$LVL$pH.CalEmission - E$LVL$pH_CorrectedEmission)/E$PH_COEF$gain))*1000^-1
+  E$LVL<-E$LVL[order(E$LVL$Tick),]
   E$LVL$file<-E$file
   E
 }
