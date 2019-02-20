d18703f94751bc13646e21342842899600d6d026
JARS3N <JARS3N@users.noreply.github.com>
2019-02-20 00:03:27 -0500

    Create change_pH_coef.R

---
+++ R/change_pH_coef.R
@@ -0,0 +1,26 @@
+change_pH_coef <- function(file, PH_B = NULL, PH_C = NULL) {
+  #c3 = ph_b,c4=ph_c
+  change_val <- function(var, xpath) {
+    node <- xpathApply(xml, xpath)
+    sapply(node, function(N) {
+      xmlValue(N) <- as.character(var)
+    })
+  }
+  paths <- Reduce(
+    'paste0',
+    list(
+      "//Item[Key='pH']//Value//AnalyteCalibration//GainEquation//",
+      c("C3", "C4"),
+      "/text()"
+    )
+  )
+  
+  xml <- XML::xmlTreeParse(file, useInternalNodes = T)
+  
+  Map(change_val, c(PH_B, PH_C), paths)
+  
+  name_xml <- gsub("asyr", "xml", file)
+  name_asyr <- gsub("[.]asyr", "_corrected_5.asyr", file)
+  XML::saveXML(xml, gsub("asyr", "xml", file))
+  R.utils::gzip(name_xml, name_asyr)
+}
